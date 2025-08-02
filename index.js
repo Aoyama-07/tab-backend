1ef745c3012c0b127fc60d7792b1faa05c811331
申栋辉 <173649541@qq.com>
2025-08-02 22:10:47 +0800

    Day1: init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (_req, res) => res.send('Day1: Node.js on Termux'));
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+app.get('/',(_req, res) => res.send('Day1:Node.js on Termux'));
+app.listen(port, () => console.log('Server running at http://localhost:${port}`));
